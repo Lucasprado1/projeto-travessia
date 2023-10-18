ed3fb23a7cd623bcf1b566a722a240a59246558a
Lprd1 <74442630+Lucasprado1@users.noreply.github.com>
2023-10-18 21:48:01 +0000

    new front now working but missing operation name and date

--- projeto-travessia-frontend/src/app/modules/generate-reports/generate-reports.component.ts
+++ projeto-travessia-frontend/src/app/modules/generate-reports/generate-reports.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-interface Food {
+import { GenerateReportsService } from './generate-reports';
+interface Operation {
   value: string;
   viewValue: string;
 }
-
-
 @Component({
   selector: 'app-generate-reports',
   templateUrl: './generate-reports.component.html',
@@ -14,14 +13,21 @@ export class GenerateReportsComponent {
 
   uploadedFile:  File | null = null;
   fileName: any;
+  selectedDate: Date | null = null;
+  selectedOperation: any;
+  public isGenerating: boolean = false;
+  public reportGenerated: boolean = false;
+  public reportDownloadLink: string = '';
+  constructor(private reportGeneratorService: GenerateReportsService) {
+   
+  }
 
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Raposo'},
-    {value: 'pizza-1', viewValue: 'Sei la'},
-    {value: 'tacos-2', viewValue: 'Outro'},
+  operations: Operation[] = [
+    {value: 'raposo', viewValue: 'Raposo'},
+    {value: 'ibira', viewValue: 'Ibirapitanga'},
+    {value: 'atmosfera', viewValue: 'Atmosfera'},
   ];
   
-
 
   onFileSelected(event: any) {
     // Quando um arquivo é selecionado no campo de upload
@@ -31,10 +37,8 @@ export class GenerateReportsComponent {
       console.log(this.uploadedFile.name)
       this.fileName = this.uploadedFile?.name;
     }
-    
   }
 
-  
   triggerFileInput() {
     console.log("clicou")
     // Aciona o input de arquivo quando a segunda div é clicada
@@ -42,6 +46,51 @@ export class GenerateReportsComponent {
     if (fileInput) {
       fileInput.click();
     }
+  }
+
+  generateReport(): void {
+    console.log(this.selectedOperation);
+    console.log(this.selectedDate);
+    this.isGenerating = true;
+    if (!this.uploadedFile) {
+      // Certifique-se de que um arquivo foi carregado antes de enviar
+      alert('Por favor, selecione um arquivo Excel antes de enviar.');
+      return;
+    }
+
+    // Chame o serviço para fazer o upload do arquivo
+    this.reportGeneratorService.uploadFile(this.uploadedFile).subscribe(
+      response => {
+        // Trate a resposta do backend, se necessário
+        console.log('Resposta do servidor:', response);
+        this.reportGenerated = true;
+        this.isGenerating = false;
+        // Limpe ou atualize a interface do usuário, se necessário
+      },
+      error => {
+        console.error('Erro ao fazer o upload do arquivo:', error);
+        // Lide com erros, se necessário
+      }
+    );
+  }
+
+  downloadReport(){
+    // Implement report generation logic here
+    // After generating the report, set reportGenerated to true and provide the download link.
+    this.reportGeneratorService.generateReport().subscribe(response => {
+      // Crie um objeto URL para o arquivo baixado
+      const blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = window.URL.createObjectURL(blob);
+
+      // Crie um link para fazer o download do arquivo
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'arquivo.xlsx';
+      a.click();
+
+      // Libere a URL criada para o objeto Blob
+      window.URL.revokeObjectURL(url);
+    });
   }
   
 }
